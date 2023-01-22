2b95404b26ad57f97b382c293554bb66ed729025
eunseopjung <122076337+eunseopjung@users.noreply.github.com>
2023-01-22 18:46:21 +0900

    N과 M (4).py

--- baekjoon.code/백트래킹/N과 M (4).py
+++ baekjoon.code/백트래킹/N과 M (4).py
@@ -1,31 +1,48 @@
 import sys
 input = sys.stdin.readline
 
+# s = []
+#
+#
+# def check(a, i):
+#     if a == 0:
+#         return 1
+#     if s[a - 1] > i:
+#         return 0
+#     for j in range(a - 1):
+#         if s[j] > s[j + 1]:
+#             return 0
+#     return 1
+#
+#
+# def dfs():
+#     if len(s) == M:
+#         print(' '.join(map(str, s)))
+#         return
+#
+#     for i in range(1, N+1):
+#         if check(len(s), i):
+#             s.append(i)
+#             dfs()
+#             s.pop()
+#
+#
+# N, M = map(int, input().split())
+# dfs()
+
 s = []
 
 
-def check(a, i):
-    if a == 0:
-        return 1
-    if s[a - 1] > i:
-        return 0
-    for j in range(a - 1):
-        if s[j] > s[j + 1]:
-            return 0
-    return 1
-
-
-def dfs():
+def dfs(start):
     if len(s) == M:
         print(' '.join(map(str, s)))
         return
 
-    for i in range(1, N+1):
-        if check(len(s), i):
-            s.append(i)
-            dfs()
-            s.pop()
+    for i in range(start, N+1):
+        s.append(i)
+        dfs(i)
+        s.pop()
 
 
 N, M = map(int, input().split())
-dfs()
+dfs(1)
