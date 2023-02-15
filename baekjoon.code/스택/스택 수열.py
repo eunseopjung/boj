8bf3cae7140b15f7ef1f6ff3ae0b328c995f04a2
eunseopjung <122076337+eunseopjung@users.noreply.github.com>
2023-02-15 21:03:32 +0900

    스택 수열.py

---
+++ baekjoon.code/스택/스택 수열.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+stack = []
+ans = []
+cnt = 1
+temp = True
+for i in range(n):
+    num = int(input())
+    while cnt <= num:
+        stack.append(cnt)
+        ans.append('+')
+        cnt += 1
+    if stack[-1] == num:
+        stack.pop()
+        ans.append('-')
+    else:
+        temp = False
+
+if temp is False:
+    print('NO')
+else:
+    for i in ans:
+        print(i)
