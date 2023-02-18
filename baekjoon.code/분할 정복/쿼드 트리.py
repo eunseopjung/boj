c9b08c7b1c3d85be6adae4c1518c06c7b939993e
eunseopjung <122076337+eunseopjung@users.noreply.github.com>
2023-02-18 14:43:23 +0900

    쿼드 트리.py

--- baekjoon.code/분할 정복/쿼드 트리.py
+++ baekjoon.code/분할 정복/쿼드 트리.py
@@ -2,9 +2,7 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-paper = [list(map(int, input().split())) for _ in range(n)]
-
-result = []
+paper = [list(map(int, input().strip())) for _ in range(n)]
 
 
 def fx(x, y, N):
@@ -12,17 +10,19 @@ def fx(x, y, N):
     for i in range(x, x+N):
         for j in range(y, y+N):
             if color != paper[i][j]:
+                print('(', end='')
                 fx(x, y, N//2)
                 fx(x, y+N//2, N//2)
                 fx(x+N//2, y, N//2)
                 fx(x+N//2, y+N//2, N//2)
+                print(')', end='')
                 return
     if color:
-        result.append(1)
+        print(1, end='')
+        return
     else:
-        result.append(0)
+        print(0, end='')
+        return
 
 
 fx(0, 0, n)
-print(result.count(0))
-print(result.count(1))
