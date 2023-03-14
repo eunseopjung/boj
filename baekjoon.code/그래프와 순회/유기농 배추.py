22b319db25fd42b238d2ecffe6412420409e4620
jues <jues0379@gmail.com>
2023-03-14 14:48:12 +0900

    유기농 배추.py

---
+++ baekjoon.code/그래프와 순회/유기농 배추.py
@@ -0,0 +1,43 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
+
+
+def bfs(graph, a, b):
+    queue = deque()
+    queue.append((a, b))
+    graph[a][b] = 0
+    count = 1
+
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                continue
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = 0
+                queue.append((nx, ny))
+                count += 1
+    return count
+
+
+t = int(input())
+for _ in range(t):
+    m, n, k = map(int, input().split())
+    graph = [[0 for _ in range(m)] for _ in range(n)]
+    for i in range(k):
+        x, y = map(int, input().split())
+        graph[y][x] = 1
+
+    cnt = []
+    for i in range(n):
+        for j in range(m):
+            if graph[i][j] == 1:
+                cnt.append(bfs(graph, i, j))
+
+    print(len(cnt))
