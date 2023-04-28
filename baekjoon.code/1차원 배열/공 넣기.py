8b8a12444abc1239cc5330c9ed8bdbd68a65c6a6
eunseopjung <122076337+eunseopjung@users.noreply.github.com>
2023-04-28 21:34:50 +0900

    공 넣기.py

---
+++ baekjoon.code/1차원 배열/공 넣기.py
@@ -0,0 +1,13 @@
+import sys
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+basket = [0 for i in range(n)]
+
+for _ in range(m):
+    i, j, k = map(int, input().split())
+    for l in range(i - 1, j):
+        basket[l] = k
+
+print(*basket)
