de9d1209bd316bdfba3efbe710d3abd2704802f8
eunseopjung <122076337+eunseopjung@users.noreply.github.com>
2023-04-28 22:38:52 +0900

    바구니 뒤집기.py

---
+++ baekjoon.code/1차원 배열/바구니 뒤집기.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+basket = [i+1 for i in range(n)]
+
+for _ in range(m):
+    i, j = map(int, input().split())
+    basket[i - 1:j] = list(reversed(basket[i - 1:j]))
+
+print(*basket)
